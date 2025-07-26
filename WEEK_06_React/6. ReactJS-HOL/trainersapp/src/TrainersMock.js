3c22a63449c4d04d82da55b147c5f8d425fd5217
Sushant-Zambare <126985907+Sushant-Zambare@users.noreply.github.com>
2025-07-26 12:56:14 +0530

    Add files via upload

---
+++ WEEK_06_React/6. ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,29 @@
+import Trainer from "./Trainer";
+const trainersMock = [
+    new Trainer(
+        'sush-01',
+        'Sushant Zambare',
+        'zambaresushant@cognizant.com',
+        '84576519354',
+        'JAVA',
+        ['C','SQL Server', 'React', 'JAVA core']
+    ),
+    new Trainer(
+        'yash-01',
+        'Yask Kolhi',
+        'kolhiyash@cognizant.com',
+        '9587425475',
+        '.NET',
+        ['C#','SQL Server', 'React', '.NET core']
+    ),
+    new Trainer(
+        'shubham-02',
+        'Shubham kumar',
+        'kumarshubham@cognizant.com',
+        '8695745215',
+        'Python',
+        ['Java','SQL Server', 'React', '.Python']
+    ),
+]
+
+export default trainersMock;
