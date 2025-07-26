4112885bb4bfe98fa1a9baadd69810e84edfd667
Sushant-Zambare <126985907+Sushant-Zambare@users.noreply.github.com>
2025-07-26 12:53:09 +0530

    Add files via upload

---
+++ WEEK_06_React/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./components/CalculateScore";
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Sushant"}
+      School={"Rustomjie International School"}
+      total={284}
+      goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
